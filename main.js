3f1d0a3980ba8ee066e970157cc7bb6c3ee022bf
mitskixw <143209910+mitskixw@users.noreply.github.com>
2024-10-24 08:26:01 -0300

    w

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criarCartao (categotia, pergunta, reposta){
+function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
