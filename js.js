617616be7bfc7f73bf486442ad82123e0f3ecc58
mitskixw <143209910+mitskixw@users.noreply.github.com>
2024-11-07 08:42:11 -0300

    2

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function criaCartao (categoria,pergunta,resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML= `<div class="conteudo-cartao">
+cartao.innerHTML= `<div class="conteudo-cartao" onclick = "mostraResposta(this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p> ${pergunta} </p>
@@ -15,13 +15,13 @@ function criaCartao (categoria,pergunta,resposta){
     container.appendChild(cartao);
     }
     
-    function mostraResposta(){
+    function mostraResposta(card){
 const resposta= document.querySelector('.resposta-cartao');
-if(resposta.style.display === 'none')}
+if(resposta.style.display === 'none'){
     resposta.style.display = 'block'
-   
- {
+}
     else {
     resposta.style.display = 'none'
 
 }
+    }
